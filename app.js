3aed3db498e341b95aef1828182210ca75295514
msalam <ms17alam@gmail.com>
2018-06-10 15:54:19 -0400

    whtever

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
+
+// BUDGET CONTROLLER
+
 var budgetController = (function(){
-	var x = 23;
-	var add = function(a){
-		return x + a;
-	}
-	return{
-		publicTest: function(b){
-			console.log(add(b));
-		}
-	}
+	
 })();
 
 
+// UI CONTROLLER
 var UIController = (function(){
 	
 	// some code
 })();
 
 
+// GLOBAL APP CONTROLLER
+
 var controller = (function(budgetCtrl, UICtrl){
 	
-	
+	document.querySelector
 	
 })(budgetController, UIController);
