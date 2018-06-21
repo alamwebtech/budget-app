82a3e6112033f02b453de4d2288c2306fcae5956
msalam <ms17alam@gmail.com>
2018-06-21 12:00:26 -0400

    function constructions

--- app.js
+++ app.js
@@ -3,6 +3,13 @@
 
 var budgetController = (function(){
 	
+	var Expense = function(id, description, value){
+		this.id = id;
+		this.description = description;
+		this.value = value;
+		
+	}
+	
 })();
 
 
